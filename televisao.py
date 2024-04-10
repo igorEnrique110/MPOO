569d1b91db915dc388619c5b1bee2ff2431712f2
Igor Enrique <igorenriquepereiradelima@gmail.com>
2024-04-10 10:30:23 -0300

    conclusao

--- televisao.py
+++ televisao.py
@@ -1,6 +1,7 @@
 class Televisao:
     def __init__(self, estado):
         self.volume = 10
+        self.canal = 5
         self.estado = estado
 
     def modeOnOff(self):
@@ -16,19 +17,31 @@ class Televisao:
 
         print(self.volume)
 
-
     def diminuirVolume(self):
         self.volume = self.volume - 1
 
         print(self.volume)
 
+    def aumentarCanal(self):
+        self.canal = self.canal + 1
+
+        print(self.canal)
+
+    def diminuirCanal(self):
+        self.canal = self.canal - 1
+
+        print(self.canal)
+
 
 
 
 televisao1 = Televisao("ligar")
+
 televisao1.modeOnOff()
 print(televisao1.estado)
 #print(televisao1.aumentarVolume())
 print(televisao1.diminuirVolume())
-
+# print(televisao1.aumentarCanal())
+# print(televisao1.aumentarCanal())
+print(televisao1.diminuirCanal())
 
